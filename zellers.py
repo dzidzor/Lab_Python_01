c59e8e928b0979e46b5e2908d388b1eb414cf8b8
AITI Student <aiti-student@aiti-093-PC.(none)>
2012-06-20 07:05:38 -0400

    results for lab 1

---
+++ zellers.py
@@ -0,0 +1,16 @@
+f_name=raw_input('Enter your first name:')
+l_name=raw_input('Enter your last name:')
+print('Enter your date of birth:')
+print('NB. Year starts from March i.e March=1, April=2....February=12')
+A=raw_input('Month?')
+B=raw_input('Day?'))
+C=raw_input('Year? [please enter last 2 digits only]')
+D=raw_input('Century?')
+print f_name +" "+ l_name +" "+ "was born on"+ " "+ A+" "+B+"," +C
+W = (13*A - 1) / 5
+X=C/4
+Y=D/4
+Z = W + X + Y + B + C - 2*D
+R = Z%7
+if R=0
+
